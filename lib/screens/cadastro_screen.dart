929f8f92b21a315489d7bea1abcacdfe9121a342
Felipe <feliperegoc@gmail.com>
2024-10-10 16:53:30 -0300

    prints

--- lib/screens/cadastro_screen.dart
+++ lib/screens/cadastro_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import '../services/auth_service.dart';
 import 'login_screen.dart'; // Importar a tela de login
 
